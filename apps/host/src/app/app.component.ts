dff18f631ee7993a4a8a0c997c8fa8f87e611324
enochgao <enochgao@qq.com>
2023-11-30 17:29:50 +0800

    chore: chore

--- apps/host/src/app/app.component.ts
+++ apps/host/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gy-test-root',
+  selector: 'nx-test-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
